fc6d6e7bc1715b98b4238b289fc2056df4793a12
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-11-07 23:05:25 +0530

    expo react native setup

---
+++ app/App.js
@@ -0,0 +1,9 @@
+import StackNavigator from "./navigation/StackNavigator";
+
+export default function App() {
+  return (
+    <>
+      <StackNavigator />
+    </>
+  );
+}
---
+++ app/navigation/StackNavigator.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
