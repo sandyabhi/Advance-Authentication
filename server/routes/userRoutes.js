9c46593f87decb612eddae8e3e9e1a76b638b20c
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-11-07 21:22:30 +0530

    server side code completed

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  login,
+  logout,
+  register,
+  verify,
+  addTask,
+  updateTask,
+  getMyProfile,
+  updateProfile,
+  removeTask,
+  updatePassword,
+  forgetPassword,
+  resetPassword,
+} from "../controllers/userControllers.js";
+import { isAuthenticated } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.route("/register").post(register);
+router.route("/verify").post(isAuthenticated, verify);
+router.route("/login").post(login);
+router.route("/logout").post(logout);
+router.route("/me").get(isAuthenticated, getMyProfile);
+
+router.route("/updateprofile").put(isAuthenticated, updateProfile);
+router.route("/updatepassword").put(isAuthenticated, updatePassword);
+router.route("/forgetpassword").post(forgetPassword);
+router.route("/resetpassword").put(resetPassword);
+
+router.route("/newtask").post(isAuthenticated, addTask);
+router
+  .route("/task/:taskId")
+  .get(isAuthenticated, updateTask)
+  .delete(isAuthenticated, removeTask);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,46 @@
-import { app } from "./app.js";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/database.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cloudinary from "cloudinary";
+
+const app = express();
+
+app.use(cors());
+
+dotenv.config();
+connectDatabase();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    useTempFiles: true,
+  })
+);
+
+app.use("/api/v1", userRoutes);
+
+// ------------------------------------------------------------------------
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT", PORT);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is working");
 });
